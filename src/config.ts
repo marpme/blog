26c849765714d41852f66aa29ae96e3a5f2effbb
marpme <7430964+marpme@users.noreply.github.com>
2023-10-30 19:44:02 +0100

    chore: beautify page with some smaller additions

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "marpmes' blog";
+export const SITE_TITLE = "marpme";
 export const SITE_DESCRIPTION =
   "Welcome to my awesome blog, talking about web technologies and various related topics";
 export const TWITTER_HANDLE = "@marpme_";
